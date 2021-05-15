13f61e8b10671b4eca7f0106622869be0351b47b
Hwang sujeong <401sujeong@gmail.com>
2021-05-16 02:46:53 +0900

    login and signup commit - activity_main.xml / MainActivity,java 겹쳐서 activity_logout.xml로 바꾸고 java파일은 결합한 상태.

--- cosu/app/src/main/java/com/example/cosu_pra/MainActivity.java
+++ cosu/app/src/main/java/com/example/cosu_pra/MainActivity.java
@@ -19,6 +19,7 @@ import com.example.cosu_pra.DTO.StudyPost;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -26,18 +27,85 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+//--sujeong login and signup--//
+import android.app.ProgressDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth firebaseAuth;
+    //찬영 개발
     HelpPosting helpPost;
     Map<String, ProjectPost> posts;
+    //
+    Button memberWithdrawal,logOut;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Testing testing = new Testing();
+        setContentView(R.layout.activity_logout);
+        Testing testing = new Testing();     //찬영 개발
 
+        memberWithdrawal =(Button) findViewById(R.id.memberWithdrawal);
+        logOut = (Button) findViewById(R.id.logOut);
+        progressDialog = new ProgressDialog(this); //conncect Function
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        memberWithdrawal.setOnClickListener(this);
+        logOut.setOnClickListener(this);
+
+    }
+    private void Logout(){
+
+    }
+    private void deleteUser(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        progressDialog.setMessage("Deleting User. please wait a moment please...");
+        progressDialog.show();
+
+        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Your account has been deleted.", Toast.LENGTH_LONG).show();
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }else {
+                    Toast.makeText(getApplicationContext(),"Please try again",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.memberWithdrawal:
+                deleteUser();
+                break;
+            case R.id.logOut:
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+
+
+    }
 }
